e084bd3951ad2aecb88d5ae713d8f1804e8f800e
Caro <cslondo90@gmail.com>
2020-06-04 19:33:58 -0500

    versión 3

---
+++ index.js
@@ -0,0 +1,20 @@
+function continuarPago() {
+    document.getElementById('ventana1').style.display = 'none';
+    document.getElementById('ventana2').style.display = 'block';
+    }
+
+
+function continuarPago2(cardNum)
+{
+    var numbers = /^[0-9]+$/;
+    
+    if(cardNum.value.match(numbers)){
+        document.getElementById("ventana2").style.display="none";
+        document.getElementById("ventana3").style.display="block";
+    }else{
+        alert('Por favor, ingresa solamente números.');
+
+    }
+    
+}
+
