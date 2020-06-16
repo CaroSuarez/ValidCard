3f03a70e9c45ed216e6ed49cb60fb3c50e934c21
Caro <cslondo90@gmail.com>
2020-06-16 18:49:02 -0500

    versión junio 16

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-
-cB=document.getElementById("continuarButton")
-vB=document.getElementById("validationButton")
-
-cB.addEventListener("click", continuarPago);
-vB.addEventListener("click", continuarPago2);
-
-pruebaMask=vB.addEventListener("click", maskify);
-document.getElementById("demo1").innerHTML=pruebaMask;
-
-function continuarPago() {
-    document.getElementById('ventana1').style.display = 'none';
-    document.getElementById('ventana2').style.display = 'block';
-}
-
-function continuarPago2() {
-    document.getElementById('ventana2').style.display = 'none';
-    document.getElementById('ventana3').style.display = 'block';
-}
-
-
+/*Importing validator functions*/
 import validator from './validator.js';
-
 console.log(validator);
 
+/*Getting elements from HTML*/
+var continueButton=document.getElementById("continueButton");
+var validationButton=document.getElementById("validationButton");
+var inputNumber=document.getElementById("inputNumber");
 
+/*Envents Listeners from HTML buttons*/
+continueButton.addEventListener("click", continuePayment);
+validationButton.addEventListener("click", continuePayment2);
+
+/*Functions*/
+function continuePayment() {
+    document.getElementById('window1').style.display = 'none';
+    document.getElementById('window2').style.display = 'block';
+}
+
+function continuePayment2() {
+    /*Printing masked number*/
+    var maskedNumber=validator.maskify(inputNumber.value);
+    var p1 = document.createElement("P");
+    var result1= document.createTextNode(maskedNumber);
+    p1.appendChild(result1);
+    document.getElementById("results").appendChild(p1)
+
+    /*Changing window*/    
+    document.getElementById('window2').style.display = 'none';
+    document.getElementById('window3').style.display = 'block';
+}
--- validator.js
+++ validator.js
@@ -1,19 +1,19 @@
 const validator = {
   maskify: function(creditCardNumber) {
-     var n=creditCardNumber.length;
-     
-     if(n<5){
-       return creditCardNumber; 
-     }else{    	
-       var x1=creditCardNumber.substr(-4,4);
-       var x2=creditCardNumber.substr(0,n-4).replace(/[0-9]/g,"*");
-       var x3=x2.concat(x1); 
-       return x3;
-     }
- }
-};
- 
-
+    var n=creditCardNumber.length;
+    
+    if(n<5){
+      return creditCardNumber; 
+      
+    }else{    	
+        var x1=creditCardNumber.substr(-4,4);
+        var x2=creditCardNumber.substr(0,n-4).replace(/\w/g,"*");
+        var x3=x2.concat(x1); 
+        return x3;
+        
+      }
+    }
+  };
 
 export default validator;
 
